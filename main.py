c547910c0396e9608d4d58198224b94f4355fe6d
Javier Marcon <javiermarcon@gmail.com>
2019-07-21 23:58:45 -0300

    Background color change on 5, 6 and 7 seconds

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from time import strftime
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import BooleanProperty
+from kivy.properties import BooleanProperty, ListProperty
 
 class RootWidget(BoxLayout):
     pass
@@ -11,10 +11,12 @@ class RootWidget(BoxLayout):
 class toastmastersclockApp(App):
     sw_started = BooleanProperty(False)
     sw_seconds = 0
+    background = ListProperty()
 
     def update_time(self, nap):
         if self.sw_started:
             self.sw_seconds += nap
+            self._check_elapsed_time()
         minutes, seconds = divmod(self.sw_seconds, 60)
         self.root.ids.stopwatch.text = (
             '[b]%02d:%02d.[size=40]%02d[/size][/b]' %
@@ -22,6 +24,18 @@ class toastmastersclockApp(App):
              int(seconds * 100 % 100))
         )
         self.root.ids.time.text = strftime('%H:%M:%S')
+
+
+    def _check_elapsed_time(self):
+        """Checks the elapsed time and changes the backgroun accordingly"""
+        colors = {
+            5 : [0, 1, 0, 1],
+            6 : [1, 1, 0, 1],
+            1 * 7: [1, 0, 0, 1],
+        }
+        for seconds in colors:
+            if int(self.sw_seconds) == seconds:
+                self.background = colors[seconds]
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
@@ -37,9 +51,11 @@ class toastmastersclockApp(App):
             self.root.ids.start_stop.text = 'Start'
             self.sw_started = False
         self.sw_seconds = 0
+        self.background = [1, 1, 1, 1]
 
     def build(self):
         root = RootWidget()
+        self.reset()
         return root
 
 if __name__ == '__main__':

e07225579128af25da6f023d659a8b908df5a3e7
Javier Marcon <javiermarcon@gmail.com>
2019-07-28 20:18:03 -0300

    removed the space between the number and the identifier in the time

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def display_time(seconds, granularity=2):
             seconds -= value * count
             if value == 1:
                 name = name.rstrip('s')
-            result.append("{} {}".format(value, name))
+            result.append("{}{}".format(value, name))
     return ', '.join(result[:granularity])
 
 class RootWidget(BoxLayout):

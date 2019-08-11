f63ed26c791d6b52ddf1bcee615a17a12e227705
Javier Marcon <javiermarcon@gmail.com>
2019-08-11 08:44:40 -0300

    feature: category and subcategory to select

--- src/main.py
+++ src/main.py
@@ -16,12 +16,21 @@ from kivy.properties import BooleanProperty, ListProperty, StringProperty
 from kivy.uix.spinner import Spinner
 
 colors = { "green": [0, 1, 0, 1], "yellow": [1, 1, 0, 1], "red": [1, 0, 0, 1] }
-speechTypes = OrderedDict()
-speechTypes["Impromptu"] = { 60 : "green", 90 : "yellow", 120: "red" }
-speechTypes["Speech"] = { 5 * 60 : "green", 6 * 60 : "yellow", 7 * 60: "red" }
-speechTypes["Evaluation"] = { 120 : "green", 150 : "yellow", 180: "red" }
-speechTypes["Sprint"] = { 8 * 60 : "green", 9 * 60 : "yellow", 10 * 60: "red" }
-speechTypes["Test"] = { 2 : "green", 3 : "yellow", 4: "red" }
+speechTypes = {
+    'Speech' : {
+            "Impromptu": { 60 : "green", 90 : "yellow", 120: "red" },
+            "Speech": { 5 * 60 : "green", 6 * 60 : "yellow", 7 * 60: "red" },
+            "Evaluation": { 120 : "green", 150 : "yellow", 180: "red" }
+            },
+    'Sprint': {
+            "Daily":  { 10 * 60 : "green", 12.5 * 60 : "yellow", 15 * 60: "red" },
+            "Sprint": { 8 * 60 : "green", 9 * 60 : "yellow", 10 * 60: "red" },
+            "Demo": { 30 * 60 : "green", 35 * 60 : "yellow", 40 * 60: "red" }
+            },
+    'Test': {
+            "Test": { 2 : "green", 3 : "yellow", 4: "red" }
+            }
+        }
 
 intervals = (
     ('w', 604800),  # 60 * 60 * 24 * 7
@@ -50,16 +59,9 @@ class toastmastersclockApp(App):
     sw_started = BooleanProperty(False)
     sw_seconds = 0
     background = ListProperty()
-    spinnerValues = ListProperty()
-    spinnerText = StringProperty()
-
-    def _get_spinner_values(self):
-        labels = []
-        for category in speechTypes.keys():
-            minval = display_time(min(speechTypes[category]))
-            maxval = display_time(max(speechTypes[category]))
-            labels.append('{} : {} - {}'.format(category, minval, maxval))
-        return labels
+    categoryValues = ListProperty()
+    defaultSpeechName = '< Select >'
+    speechNameValues = ListProperty([defaultSpeechName])
 
     def update_time(self, nap):
         if self.sw_started:
@@ -75,11 +77,13 @@ class toastmastersclockApp(App):
 
     def _check_elapsed_time(self):
         """Checks the elapsed time and changes the backgroun accordingly"""
-        category_string = self.root.ids.spinner_1.text.split(':')[0].strip()
-        selectedCategory = speechTypes[category_string]
-        for seconds in selectedCategory:
-            if int(self.sw_seconds) == seconds:
-                self.background = colors[selectedCategory[seconds]]
+        category_string = self.root.ids.category.text
+        speech_string = self.root.ids.speechName.text.split(':')[0].strip()
+        if category_string in speechTypes.keys() and speech_string in speechTypes[category_string].keys():
+            selectedCategory = speechTypes[category_string][speech_string]
+            for seconds in selectedCategory:
+                if int(self.sw_seconds) == seconds:
+                    self.background = colors[selectedCategory[seconds]]
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
@@ -97,15 +101,21 @@ class toastmastersclockApp(App):
         self.sw_seconds = 0
         self.background = [1, 1, 1, 1]
 
-    #def spinnerChanged(self, textValue):
-    #    print(textValue)
+    def category_changed(self, category_name):
+        labels = []
+        if category_name in speechTypes.keys():
+            speeches = sorted(speechTypes[category_name])
+            for speechName in speeches:
+                minval = display_time(min(speechTypes[category_name][speechName]))
+                maxval = display_time(max(speechTypes[category_name][speechName]))
+                labels.append('{} : {} - {}'.format(speechName, minval, maxval))
+            self.speechNameValues = labels
+            self.root.ids.speechName.text = self.defaultSpeechName
 
     def build(self):
         self.icon = 'assets/icon.png'
         root = RootWidget()
-        spinner_values = self._get_spinner_values()
-        self.spinnerValues = spinner_values
-        self.spinnerText = spinner_values[0]
+        self.categoryValues = sorted(speechTypes.keys())
         self.reset()
         return root
 

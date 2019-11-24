a84acdeec3a71fab59bef9acede5454df012b2d9
Javier Marcon <javiermarcon@gmail.com>
2019-11-24 16:00:01 -0300

    feature: change settings colorpicker

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from kivy.config import Config
 Config.set('graphics', 'resizable', True)
 
 from time import strftime
-from collections import OrderedDict
+import json
 
 from kivy.app import App
 from kivy.clock import Clock
@@ -14,21 +14,27 @@ from kivy.utils import platform
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import BooleanProperty, ListProperty, StringProperty
 from kivy.uix.spinner import Spinner
+from kivy.uix.settings import Settings
+from kivy.uix.colorpicker import ColorPicker
+from kivy.utils import get_color_from_hex
+from kivy.uix.settings import SettingsWithSidebar
+from kivy.core.window import Window
 
-colors = { "green": [0, 1, 0, 1], "yellow": [1, 1, 0, 1], "red": [1, 0, 0, 1] }
+from settingscolorpicker import SettingColorPicker
+
 speechTypes = {
     'Speech' : {
-            "Impromptu": { 60 : "green", 90 : "yellow", 120: "red" },
-            "Speech": { 5 * 60 : "green", 6 * 60 : "yellow", 7 * 60: "red" },
-            "Evaluation": { 120 : "green", 150 : "yellow", 180: "red" }
+            "Impromptu": { 60 : "min", 90 : "med", 120: "max" },
+            "Speech": { 5 * 60 : "min", 6 * 60 : "med", 7 * 60: "max" },
+            "Evaluation": { 120 : "min", 150 : "med", 180: "max" }
             },
     'Sprint': {
-            "Daily":  { 10 * 60 : "green", 12.5 * 60 : "yellow", 15 * 60: "red" },
-            "Sprint": { 8 * 60 : "green", 9 * 60 : "yellow", 10 * 60: "red" },
-            "Demo": { 30 * 60 : "green", 35 * 60 : "yellow", 40 * 60: "red" }
+            "Daily":  { 10 * 60 : "min", 12.5 * 60 : "med", 15 * 60: "max" },
+            "Sprint": { 8 * 60 : "min", 9 * 60 : "med", 10 * 60: "max" },
+            "Demo": { 30 * 60 : "min", 35 * 60 : "med", 40 * 60: "max" }
             },
     'Test': {
-            "Test": { 2 : "green", 3 : "yellow", 4: "red" }
+            "Test": { 2 : "min", 3 : "med", 4: "max" }
             }
         }
 
@@ -55,6 +61,9 @@ def display_time(seconds, granularity=2):
 class RootWidget(BoxLayout):
     pass
 
+class SettingsClass(SettingsWithSidebar):
+    pass
+
 class toastmastersclockApp(App):
     sw_started = BooleanProperty(False)
     sw_seconds = 0
@@ -75,15 +84,20 @@ class toastmastersclockApp(App):
         )
         self.root.ids.time.text = strftime('%H:%M:%S')
 
+    def _get_config_colors(self):
+        '''Gets the colors configured in the settings'''
+        return {color: self.config.get('screen_colors', color) for color in self._default_colors()}
+
     def _check_elapsed_time(self):
         """Checks the elapsed time and changes the backgroun accordingly"""
+        colors = self._get_config_colors()
         category_string = self.root.ids.category.text
         speech_string = self.root.ids.speechName.text.split(':')[0].strip()
         if category_string in speechTypes.keys() and speech_string in speechTypes[category_string].keys():
             selectedCategory = speechTypes[category_string][speech_string]
             for seconds in selectedCategory:
                 if int(self.sw_seconds) == seconds:
-                    self.background = colors[selectedCategory[seconds]]
+                    self.background = get_color_from_hex(colors[selectedCategory[seconds]])
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
@@ -99,7 +113,7 @@ class toastmastersclockApp(App):
             self.root.ids.start_stop.text = 'Start'
             self.sw_started = False
         self.sw_seconds = 0
-        self.background = [1, 1, 1, 1]
+        self.background = get_color_from_hex(self._get_config_colors()['default'])
 
     def category_changed(self, category_name):
         labels = []
@@ -114,16 +128,37 @@ class toastmastersclockApp(App):
 
     def build(self):
         self.icon = 'assets/icon.png'
+        self.settings_cls = SettingsWithSidebar
         root = RootWidget()
         self.categoryValues = sorted(speechTypes.keys())
         self.reset()
         return root
+
+    def build_config(self, config):
+        config.setdefaults('screen_colors', self._default_colors())
+
+    def build_settings(self, settings):
+        settings.register_type('colorpicker', SettingColorPicker)
+        menu_colors = []
+        for color in sorted(self._default_colors().keys()):
+            menu_colors.append({'type': 'colorpicker',
+                                'title': '{} color'.format(color),
+                                'desc': '{} color background'.format(color),
+                                'section': 'screen_colors',
+                                'key': color})
+        settings.add_json_panel('display colors',
+                                self.config,
+                                data=json.dumps(menu_colors))
 
     def do_quit(self, *largs):
         # print('App quit')
         if platform == 'android':
             self.root_window.close()  # Fix app exit on Android.
         return super(toastmastersclockApp, self).stop(*largs)
+
+    def _default_colors(self):
+        ''' Definies the default colors for the application '''
+        return { "min": '#00FF00', "med": '#FFFF00', "max": '#FF0000', 'default': '#000000' }
 
 if __name__ == '__main__':
     toastmastersclockApp().run()
